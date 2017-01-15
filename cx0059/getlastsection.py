f2326e0fbc3452fd3e6de8368d1f9e204b728430
Manuel Marcano <manuelmarcano22@yahoo.com>
2017-01-14 22:25:27 -0600

    apall logs of cx0025 :octocat:

--- cx0059/getlastsection.py
+++ cx0059/getlastsection.py
@@ -7,6 +7,6 @@ ssem = fits.open('VI_SSEM_575273_2011-05-26T05:19:50.824_G475_MR_202166_Q2_hi.fi
 header1 = ssem[0].header
 header1['NAXIS2'] = 1
 
-fits.writeto('lastmedian.fits', np.median(ssem[0].data, axis=0)  ,header1)          
-mediannorm = [ i/np.max(ssem[0].data) for i in np.median(ssem[0].data,axis=0)]  
+fits.writeto('lastmedian.fits', np.median(ssem[0].data[0:80,:], axis=0)  ,header1)          
+mediannorm = [ i/np.max(ssem[0].data) for i in np.median(ssem[0].data[0:80,:],axis=0)]  
 fits.writeto('lastmediannorm.fits', mediannorm  ,header1)
