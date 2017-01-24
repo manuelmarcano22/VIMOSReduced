61d2a8c0467b540c4d2ac519bcae3f7a24ad7e5d
Manuel Marcano <manuelmarcano22@yahoo.com>
2017-01-24 16:54:29 -0600

    :octocat:

--- cx0025/createspectrawithbokeh.py
+++ cx0025/createspectrawithbokeh.py
@@ -9,7 +9,7 @@ from astropy.convolution import convolve, Box1DKernel
 import numpy as np
 
 #Get data
-srfm = fits.open('copy_SSEM.ms.fits')
+srfm = fits.open('VI_SRFM_577734_2011-06-24T05:56:42.518_G475_MR_402230_Q4_hi.fits')
 secondstar = srfm[0].data[1]
 
 #For srfm[0].header["CTYPE1"] = 'LINEAR'
@@ -20,7 +20,7 @@ cr = srfm[0].header['CRPIX1']
 
 xlist = [ refx + step*(i - cr) for i in np.arange(1, len(secondstar)+1) ]
 
-name = 'apallspectraap2cx371smoothsky'
+name = 'spectraap3cx25smoothsky'
 
 hover = HoverTool(
         tooltips=[
