a65181c106c6cecf60fd7e36c01228528e3670e8
Manuel Marcano <manuelmarcano22@yahoo.com>
2017-02-17 15:44:25 -0600

    First Try

--- cx0025/spectracx25.py
+++ cx0025/spectracx25.py
@@ -1,13 +1,32 @@
 import sys
 from pyraf import iraf
+from astropy.io import fits
 
-
-#To modify the center
+#To modify the center, low and high parameter
 center = 100
 low = -10.1
 high = 50
+
 #name of apfile
 filename = 'database/apVI_SEXM_577734_2011-06-24T05_56_42.518_G475_MR_402230_Q4_hi' 
+#name original SEXM
+fitsfile1 = 'VI_SEXM_577734_2011-06-24T05:56:42.518_G475_MR_402230_Q4_hi.fits'
+fitsfile1d = fits.open(fitsfile1)
+
+#Exposure time to multiply the image
+exptime = fitsfile1d[0].header['EXPTIME']
+op = 'im1* '+str(exptime)
+#
+iraf.stsdas()
+iraf.images.imutil()
+iraf.images.imutil.imarith(fitsfile1, '*', exptime, 'cx25sexm.fits')
+
+#Work with image
+fitsfile = 'cx25sexm.fits'
+fitsdata = fits.getdata(fitsfile)
+#Default for dispesion line is half of the image
+dispersion = fitsdata[:,fitsdata.shape[1]/2]
+#create times the observing time
 
 with open(filename) as f:
 	for lines in f:
@@ -33,7 +52,7 @@ with open(filename,'w') as f:
 iraf.noao.twodspec()
 iraf.noao.twodspec.apextract()
 #http://vivaldi.ll.iac.es/sieinvens/siepedia/pmwiki.php?n=HOWTOs.PythonianIRAF
-iraf.noao.apextract.apall.setParam('input','VI_SEXM_577734_2011-06-24T05:56:42.518_G475_MR_402230_Q4_hi.fits')
+iraf.noao.apextract.apall.setParam('input',fitsfile)
 
 #iraf.noao.twodspec.apextract.apall.setParam('lower','-5.0')
 #iraf.noao.twodspec.apextract.apall.setParam('upper','1.0')
@@ -42,6 +61,7 @@ iraf.noao.twodspec.apextract.apall.setParam('recenter','no')
 iraf.noao.twodspec.apextract.apall.setParam('resize','no')
 iraf.noao.twodspec.apextract.apall.setParam('edit','no')
 iraf.noao.twodspec.apextract.apall.setParam('trace','no')
+iraf.noao.twodspec.apextract.apall.setParam('interactive','no')
 iraf.noao.twodspec.apextract.apall.setParam('upper','1.0')
 iraf.noao.twodspec.apextract.apall.setParam('apertures','1')
 iraf.noao.apextract.apall.saveParList(filename='cx25.par')
